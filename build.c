e9d8f2ed0009b085d8207ec08d757c7e88e824b6
Vasco <true.vasco.alves@gmail.com>
2026-08-31 19:55:38 +0100

    link -ldl on Linux
    Debian 11 still has dlsym in libdl, not libc.

--- build.c
+++ build.c
@@ -59,7 +59,7 @@ queue_app(Poof_Batch *batch, int release, int app, int cpu)
 	poof_cmd_append(&cc.includes, ".", "godstack/Peak", "godstack/Term");
 	if (app == APP_VT)
 		poof_cmd_append(&cc.includes, "godstack/Rend");
-	poof_cc_append_linux(&cc, "-lutil");
+	poof_cc_append_linux(&cc, "-lutil", "-ldl");
 	poof_cc_append_macos(&cc, "-lutil");
 	if (app == APP_VT) {
 		if (!cpu) {
